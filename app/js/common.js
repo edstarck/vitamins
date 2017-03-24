b74681a0309d5100c2e3789a2473e9e2cec5248f
edstarck <webeduard@yandex.ru>
2017-03-24 19:52:58 +0300

    Added mobile menu

--- app/js/common.js
+++ app/js/common.js
@@ -137,6 +137,10 @@ $(function() {
     nextButton: '.carousel_button-next',
     prevButton: '.carousel_button-prev',
     breakpoints: {
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
       650: {
         slidesPerView: 1,
         spaceBetween: 10
@@ -174,8 +178,7 @@ $(function() {
       classie.remove( overlay, 'open' );
       classie.add( overlay, 'close' );
       $('body').css({
-        'overflow': 'auto',
-        'padding-right': '0px'
+        'overflow': 'auto'
       });
       var onEndTransitionFn = function( ev ) {
         if( support.transitions ) {
@@ -194,8 +197,7 @@ $(function() {
     else if( !classie.has( overlay, 'close' ) ) {
       classie.add( overlay, 'open' );
       $('body').css({
-        'overflow': 'hidden',
-        'padding-right': '17px'
+        'overflow': 'hidden'
       });
     }
   }
@@ -221,4 +223,145 @@ $(function() {
       McBar1.velocity("reverse", {delay: 800});
     }
   })
+})();
+
+// Advantages mobile
+$(document).ready(function(){
+  $('.slider__grid').clone().appendTo('.advantages .wrap_inner');
+});
+
+// Mobile menu
+(function(){
+  window.morulusCustomSb = (function() {
+    var nu=navigator.userAgent,
+    aus=['Mozilla','IE'],
+    // Remove event listner polyfill
+    removeEventListner = function(el, type, handler) {
+      if ( el.addEventListener ) {
+        el.removeEventListener(type, handler, false);
+      }  else if ( elem.attachEvent ) {
+        el.detachEvent("on" + type, handler);
+      } else {
+        el["on"+type] = null;
+      };
+    },
+    // Event listner polyfill
+    eventListner = function(el, type, handler, once) {
+        var realhandler = once ? function() {
+          removeEventListner(el, type, realhandler);
+        } : handler;
+        if ( el.addEventListener ) {
+          listen = el.addEventListener( type, handler, false );
+        } else if (el.attachEvent) {
+           listen = el.addEventListener( 'on'+type, handler, false );
+        } else {
+          el['on'+type] = handler;
+        }
+        return el;
+    },
+    retfalse = function() { return !!0; },
+    disableSelection = function(el) {
+        if (nu.indexOf(aus[0]) != -1) // FF
+        el.style['MozUserSelect']='none';
+        else if (nu.indexOf(aus[1]) != -1) // IE
+        eventListner(el, 'selectstart.disableTextSelect', retfalse);
+        else
+        eventListner(el, 'mousedown.disableTextSelect', retfalse);
+    },
+    enableSelection = function(el) {
+        if (nu.indexOf(aus[0]) != -1) // FF
+        el.style['MozUserSelect']='';
+        else if (nu.indexOf(aus[1]) != -1) // IE
+        removeEventListner(el, 'selectstart.disableTextSelect', retfalse);
+        else
+        removeEventListner(el, 'mousedown.disableTextSelect', retfalse);
+    }
+    return function(a) {
+
+      var
+      listen,
+      i,
+      d=document,
+      s, // Scrollable
+      n, // Custom scrollbar wrapper
+      r, // Runner
+      height, // Runner height
+      scrollTop, // Start scroll top,
+      // Scroll handler
+      handlerScroll = function(e) {
+
+        r.style.top = ( (100 - parseInt(r.style.height)) * (s.scrollTop/(s.scrollHeight-s.clientHeight)) )+'%';
+      },
+      drag = !!0,
+      screenY = 0,
+      handlerMove = function(e) {
+        var d = ((height*(scrollTop/(s.scrollHeight-s.clientHeight)))+(e.screenY-screenY));
+        if (d>height) d = height;
+        else if (d<0) d = 0;
+        // set Scroll top
+        s.scrollTop = Math.round((s.scrollHeight-s.clientHeight)*(d/height));
+      },
+      handlerUp = function(e) {
+        drag=!!0;
+        // Enable selection
+        enableSelection(s);
+        // remove listen for window move
+        removeEventListner(window, 'mousemove', handlerMove);
+        e.stopPropagation();
+      },
+      handlerDown = function(e) {
+        drag=!0;screenY=e.screenY;
+        height=s.clientHeight-parseInt(window.getComputedStyle(r).height);
+        scrollTop=s.scrollTop;
+        // Disable selection
+        disableSelection(s);
+        // Listen for window move
+        eventListner(window, 'mousemove', handlerMove);
+        eventListner(window, 'mouseup', handlerUp, true);
+        e.preventDefault();
+        return false;
+      },
+      handlerWrapDown = function(e) {
+       if(e.offsetX>n.offsetWidth-20){
+          s.scrollTop = Math.round((s.scrollHeight-s.clientHeight)*(e.offsetY/n.offsetHeight));
+          handlerDown(e);
+        }
+      },
+      n=d.createElement('div');
+      r=d.createElement('figure');
+      s=d.createElement('div');
+
+      // Wrap each insert elements
+      for (i=0;i<a.childNodes.length;i++) {
+        s.appendChild(a.childNodes[0]);
+      }
+      a.appendChild(n);
+      n.appendChild(r);
+      n.appendChild(s);
+      n.className = '-morulus-customsb';
+      // Set size of runner
+      r.style.height = (100*(s.clientHeight/s.scrollHeight)
+       .toFixed(2))+'%';
+      // Listen for scroll
+      eventListner(s,'scroll',handlerScroll);
+      // Listen for drug
+      eventListner(r,'mousedown',handlerDown);
+      eventListner(r,'mouseup',handlerUp);
+      // Listerb for drug on wrapper
+      eventListner(n,'mousedown',handlerWrapDown);
+    };
+  })();
+
+  function go(){
+    var x = window.innerWidth;
+    if(x <= 754) {
+      morulusCustomSb(document.querySelector('.nav-box'));
+      window.removeEventListener('resize', go);
+    } else if (x >= 754) {
+      window.addEventListener('resize', go);
+    }
+  }
+
+  go();
+
 })();
